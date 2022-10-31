0d74029a5eb71af590d6b7d88313d213e616c594
Fajar Lubis <fajarsidiqlubis13@gmail.com>
2022-10-31 10:34:20 +0700

    Add project files.

---
+++ LeapMotionCursorWithPoints/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Ink;
+
+using Leap;
+using System.Reflection.Emit;
+using LeapMotionCursorWithPoints;
+
+namespace LeapMotionCursorWithPoints
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Controller leap = new();
+        float windowWidth = 1400;
+        float windowHeight = 800;
+        DrawingAttributes touchIndicator = new DrawingAttributes();
+        public MainWindow()
+        {
+            InitializeComponent();
+            CompositionTarget.Rendering += Update;
+            touchIndicator.Width = 20;
+            touchIndicator.Height = 20;
+            touchIndicator.StylusTip = StylusTip.Ellipse;
+        }
+
+        protected void Update(object sender, EventArgs e)
+        {
+            paintCanvas.Strokes.Clear();
+            windowWidth = (float)this.Width;
+            windowHeight = (float)this.Height;
+
+            Leap.Frame frame = leap.Frame();
+            InteractionBox interactionBox = leap.Frame().InteractionBox;
+            leap.EnableGesture(Gesture.GestureType.TYPEKEYTAP);
+
+            for (int g = 0; g < frame.Gestures().Count; g++)
+            {
+                switch (frame.Gestures()[g].Type)
+                {
+                    case Gesture.GestureType.TYPE_KEY_TAP:
+                        MouseCursor.SendClick();
+                        break;
+                    default:
+                        //Handle unrecognized gestures
+                        break;
+                }
+            }
+
+            for (int g = 0; g < frame.Gestures().Count; g++)
+            {
+                Console.WriteLine(frame.Gestures()[g].ToString());
+            }
+
+            foreach (Pointable pointable in leap.Frame().Pointables)
+            {
+                Leap.Vector normalizedPosition =
+                    interactionBox.NormalizePoint(pointable.StabilizedTipPosition);
+                float tx = normalizedPosition.x * windowWidth;
+                float ty = windowHeight - normalizedPosition.y * windowHeight;
+
+                int alpha = 255;
+                if (pointable.TouchDistance > 0 && pointable.TouchZone != Pointable.Zone.ZONENONE)
+                {
+                    alpha = 255 - (int)(255 * pointable.TouchDistance);
+                    touchIndicator.Color = Color.FromArgb((byte)alpha, 0x0, 0xff, 0x0);
+                }
+                else if (pointable.TouchDistance <= 0)
+                {
+                    alpha = -(int)(255 * pointable.TouchDistance);
+                    touchIndicator.Color = Color.FromArgb((byte)alpha, 0xff, 0x0, 0x0);
+                }
+                else
+                {
+                    alpha = 50;
+                    touchIndicator.Color = Color.FromArgb((byte)alpha, 0x0, 0x0, 0xff);
+                }
+                StylusPoint touchPoint = new(tx, ty);
+                StylusPointCollection tips = new(new StylusPoint[] { touchPoint });
+                Stroke touchStroke = new(tips, touchIndicator);
+                paintCanvas.Strokes.Add(touchStroke);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
+    }
+}
---
+++ LeapMotionCursorWithPoints/MouseCursor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace LeapMotionCursorWithPoints
+{
+    class MouseCursor
+    {
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(UInt32 dwFlags, UInt32 dx, UInt32 dy, UInt32 dwData, IntPtr dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int x, int y);
+
+        private const UInt32 MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const UInt32 MOUSEEVENTF_LEFTUP = 0x0004;
+        private const UInt32 MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const UInt32 MOUSEEVENTF_RIGHTUP = 0x10;
+        // private const UInt32 MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        // private const UInt32 MOUSEEVENTF_MIDDLEUP = 0x0040;
+
+        public static void MoveCursor(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+
+        public static void SendUp()
+        {
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, new System.IntPtr());
+        }
+
+        public static void SendDown()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, new System.IntPtr());
+        }
+
+        public static void SendUpRight()
+        {
+            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, new System.IntPtr());
+        }
+
+        public static void SendDownRight()
+        {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, new System.IntPtr());
+        }
+
+        public static void SendClick()
+        {
+            SendDown();
+            SendUp();
+        }
+
+        public static void SendRightClick()
+        {
+            SendDownRight();
+            SendUpRight();
+        }
+    }
+}
